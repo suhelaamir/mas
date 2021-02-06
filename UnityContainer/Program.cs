fed9f8e3322694327b8a2932623aa68575dedaa8
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2021-02-06 21:53:40 +0530

    Dependency Injection using IOC

---
+++ UnityContainer/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace UnityContainer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Driver driver = new Driver(new BMW());
+
+            //driver.RunCar();
+
+            //IUnityContainer container = new UnityContainer();
+            IUnityContainer container = new UnityContainer();
+            container.RegisterType<ICar, BMW>();
+            var driver = container.Resolve<Driver>();
+            driver.RunCar();
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+    public interface ICar
+    {
+        int Run();
+    }
+
+    public class BMW : ICar
+    {
+        private int _miles = 0;
+
+        public int Run()
+        {
+            return ++_miles;
+        }
+    }
+
+    public class Ford : ICar
+    {
+        private int _miles = 0;
+
+        public int Run()
+        {
+            return ++_miles;
+        }
+    }
+
+    public class Audi : ICar
+    {
+        private int _miles = 0;
+
+        public int Run()
+        {
+            return ++_miles;
+        }
+
+    }
+    public class Driver
+    {
+        //private ICar _car = null;
+
+        //public Driver(ICar car)
+        //{
+        //    _car = car;
+        //}
+        [Dependency]
+        public ICar _car { get; set; }
+
+        public void RunCar()
+        {
+            Console.WriteLine("Running {0} - {1} mile ", _car.GetType().Name, _car.Run());
+        }
+    }
+}
