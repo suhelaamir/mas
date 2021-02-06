39c8cc24cc7ebcd82b14e2f295cda7eace1e4a3b
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2021-02-06 19:07:11 +0530

    Solida Principle

---
+++ ConsoleApplication/DemoClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication
+{
+    class DemoClass
+    {
+    }
+    class DemoTest { }
+
+    class DemoClass2
+    { 
+    }
+}
